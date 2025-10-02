9229a8f76eac7eb8b6445a709384d16e0066b9fb
amccaugh <amccaugh@gmail.com>
2025-10-02 12:59:46 -0600

    Add GPL statements

--- cryoheatflow/area.py
+++ cryoheatflow/area.py
@@ -1,3 +1,19 @@
+# This file is part of cryoheatflow
+# Copyright (C) 2025 by Adam McCaughan
+
+# cryoheatflow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 
 ### Cross-section-area measurements in meters, returns m^2
